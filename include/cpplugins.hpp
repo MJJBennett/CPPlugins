5e12252e015c386c99e483ff001af422cc5b6978
Michael Bennett <michaelbennett.dev@gmail.com>
2018-08-10 09:00:17 -0700

    First steps towards Windows cross-compatibility.

--- include/cpplugins.hpp
+++ include/cpplugins.hpp
@@ -2,11 +2,28 @@
 #define CPPLUGINS_CPPLUGINS_HPP
 
 #include <functional>
-#include <dlfcn.h>
 #include <iostream>
-#include "../lib/example1/test.h"
-#include "sample_API.hpp"
 #include <map>
+
+#ifdef _WIN32
+#define CPPLUGINS_WINDOWS
+#elif __APPLE__
+#define CPPLUGINS_APPLE
+#define CPPLUGINS_UNIX
+#elif __linux__
+#define CPPLUGINS_LINUX
+#define CPPLUGINS_UNIX
+#elif __unix__
+#define CPPLUGINS_UNIX
+#else
+#error "Your compiler or operating system is not recognized."
+#endif
+
+#ifdef CPPLUGINS_UNIX
+#include <dlfcn.h>
+#elif CPPLUGINS_WINDOWS
+#include <windows.h>
+#endif
 
 template<typename API_T>
 class Plugin {
@@ -21,21 +38,33 @@ public:
             std::cout << "Tried to load library " + _library_path + ", but it is already loaded!\n";
             return;
         }
+#ifdef CPPLUGINS_UNIX
         _library = dlopen(_library_path.c_str(), RTLD_LAZY);
+#elif CPPLUGINS_WINDOWS
+        _library = (void *)LoadLibrary(_library_path.c_str(), RTLD_LAZY);
+#endif
         if (!_library) {
             std::cout << "Could not load library " + _library_path + ": \n" << dlerror() << std::endl;
             return;
         }
+#ifdef CPPLUGINS_UNIX
         _create = reinterpret_cast<create_t> (dlsym(_library, "create"));
+#elif CPPLUGINS_WINDOWS
+        _create = reinterpret_cast<create_t> (GetProcAddress(_library, "create"));
+#endif
         if (!_create) {
             std::cout << "Error loading create function: " << dlerror() << std::endl;
-            dlclose(_library);
+            _close_unsafe(_library);
             return;
         }
+#ifdef CPPLUGINS_UNIX
         _destroy = reinterpret_cast<destroy_t > (dlsym(_library, "destroy"));
+#elif CPPLUGINS_WINDOWS
+        _destroy = reinterpret_cast<destroy_t > (GetProcAddress(_library, "destroy"));
+#endif
         if (!_destroy) {
             std::cout << "Error loading destroy function: " << dlerror() << std::endl;
-            dlclose(_library);
+            _close_unsafe(_library);
             return;
         }
         std::cout << "Successfully loaded plugin library." << std::endl;
@@ -44,17 +73,13 @@ public:
         api = _create();
         if (!api) {
             std::cout << "Error: Was unable to load API for library." << std::endl;
-            dlclose(_library);
+            _close_unsafe(_library);
         }
     }
 
     inline bool is_loaded() {
         return _library != nullptr;
     }
-
-//    inline bool set_safemode(bool mode) {
-//        _safemode = mode;
-//    }
 
     API_T * operator->() const {
         return api;
@@ -71,17 +96,24 @@ public:
         }
         if (_library) {
             std::cout << "Closing library." << std::endl;
-            dlclose(_library);
+            _close_unsafe(_library);
         }
     }
 private:
+    inline void _close_unsafe(void * lib) {
+#ifdef CPPLUGINS_UNIX
+        dlclose(lib);
+#elif CPPLUGINS_WINDOWS
+        FreeLibrary(lib);
+#endif
+    }
+
     std::map<std::string, void *> _symbols;
     void * _library = nullptr;
     std::string _library_path;
     create_t _create = nullptr;
     destroy_t _destroy = nullptr;
     API_T * api = nullptr;
-    //bool _safemode = true;
 };
 
 #endif //CPPLUGINS_CPPLUGINS_HPP
