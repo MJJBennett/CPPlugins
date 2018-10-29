8f7c9ba1fc9ec9a9a69b644db2f2708fb6795c81
Michael Bennett <michaelbennett.dev@gmail.com>
2018-10-28 18:56:54 -0700

    Fixed test. Looks like the ./ in the path is necessary.

--- test/1/BasicLoading.cpp
+++ test/1/BasicLoading.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     using namespace cpl;
-    Plugin<BasicLoading> plugin(dl_path("libTest1Lib"), Flag::None);
+    Plugin<BasicLoading> plugin(dl_path("./libTest1Lib"), Flag::None);
     if (!plugin.is_init()) return static_cast<int>(plugin.get_state());
     return 0; // Success
 }
