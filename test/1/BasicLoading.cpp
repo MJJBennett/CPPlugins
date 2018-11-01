de40e350ef390325a8bf2b7a6d280699abbd1d2a
Michael Bennett <michaelbennett.dev@gmail.com>
2018-10-31 20:36:04 -0700

    Sub-par solution to wrapping create_t.

--- test/1/BasicLoading.cpp
+++ test/1/BasicLoading.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     using namespace cpl;
-    Plugin<BasicLoading> plugin(dl_path("./libTest1Lib"), Flag::None, 5);
+    Plugin<BasicLoading, int> plugin(dl_path("./libTest1Lib"), Flag::None, 5);
     if (!plugin.is_init()) return static_cast<int>(plugin.get_state());
     std::cout << plugin->get_state() << std::endl;
     return 0; // Success
