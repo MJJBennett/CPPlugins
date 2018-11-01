0fdb0487c650af45f6fa3e1061348e148302cc50
Michael Bennett <michaelbennett.dev@gmail.com>
2018-10-31 21:16:08 -0700

    Fixed up the example_app to have a very basic series of tests. Everything seems to be good now.

--- example_app.cpp
+++ example_app.cpp
@@ -1,34 +1,62 @@
 #include <iostream>
 #include "sample_API.hpp"
 #include "cpplugins.hpp"
+#include "test/testing.hpp"
 
 int get_application_version() {
     return 4;
 }
 
-void run_tests() {
-    std::cout << "Starting testing." << std::endl;
-    std::string test_path = "./lib/test1/libTestLibrary1";
+int test_lib_1() {
+    const std::string test_path = "./lib/test1/libTestLibrary1";
+    int fails = 0;
+
+    // Create the plugin wrapper & load the library
     cpl::Plugin<sample_API> test_plugin(cpl::dl_path(test_path), cpl::Flag::None);
-    if (!test_plugin.is_init()) std::cout << "Could not initialize the library." << std::endl;
-    std::cout << static_cast<int>(test_plugin.get_state()) << std::endl;
+
+    // Test that the plugin was initialized properly
+    hard_test_eq(test_plugin.is_init(), true, "PluginIsInitialized");
+    fails += test_eq(test_plugin.get_state<int>(), 0, "PluginStateIsZero");
+
+    // Set up the test plugin
     test_plugin->set("get_application_version", cpl::make_void(get_application_version));
     test_plugin->init();
-    if (test_plugin->get_id() != "Sample_Plugin_1_Outdated") std::cout << "Failed test 1. Got: " << test_plugin->get_id() << std::endl;
-    if (test_plugin->add(4, 5) != 9) std::cout << "Failed test 2." << std::endl;
-    std::cout << "Finished testing." << std::endl;
+
+    // Test misc functions
+    fails += test_eq(test_plugin->get_id(), std::string("Sample_Plugin_1_Outdated"), "PluginGetID");
+    fails += test_eq(test_plugin->add(4, 5), 9, "PluginAdd");
+
+    return fails;
+}
+
+int test_lib_example() {
+    std::string plugin_path = "./lib/example1/libExampleLibrary";
+    int fails = 0;
+
+    // Create the plugin wrapper & load the library
+    cpl::Plugin<sample_API> plugin(cpl::dl_path(plugin_path), cpl::Flag::None);
+
+    // Test that the plugin was initialized properly
+    hard_test_eq(plugin.is_init(), true, "PluginIsInitialized");
+    fails += test_eq(plugin.get_state<int>(), 0, "PluginStateIsZero");
+
+    // Set up the test plugin
+    plugin->set("get_application_version", cpl::make_void(get_application_version));
+    plugin->init();
+
+    fails += test_eq(plugin->get_id(), std::string("Sample_Plugin_1_Outdated"), "PluginGetID");
+    fails += test_eq(plugin->process_data(5), 10, "PluginProcessData");
+
+    return fails;
 }
 
 int main() {
-//    std::string plugin_path = "./lib/example1/libExampleLibrary";
-//    cpl::Plugin<sample_API> plugin(cpl::dl_path(plugin_path), std::nullopt);
-//    plugin->set("get_application_version", cpl::make_void(get_application_version));
-//    plugin->init();
-//    std::cout << "Plugin ID: " << plugin->get_id() << std::endl;
-//    std::cout << "Plugin processes 5 into: " << plugin->process_data(5) << std::endl;
 
-    run_tests();
+    int fails = 0;
 
-    return 0;
+    fails += test_lib_1();
+    fails += test_lib_example();
+
+    return fails;
 }
 
